a3fb00554ff42431bc038b0867555bdb6fd59c0e
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-10-14 19:24:18 -0300

    feat: update HeaderView

--- FutebolHoje/Views/HeaderView.swift
+++ FutebolHoje/Views/HeaderView.swift
@@ -12,20 +12,18 @@ struct HeaderView: View
 {
     var body: some View
     {
-        // MARK: - Cabeçalho com título
-        VStack(alignment: .leading, spacing: 4)
+        VStack(alignment: .leading)
         {
             Text("Futebol Hoje")
-                .font(.largeTitle.bold())
-            
-//                Text("Domingo, 12 de outubro")
-//                    .font(.subheadline)
-//                    .foregroundColor(.secondary)
-//                    .padding(.top, 2)
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.horizontal)
         .padding(.top, 16)
-        .padding(.bottom, 16)
+        .padding(.bottom, 8)
+        .background(.ultraThinMaterial)
     }
 }
 
