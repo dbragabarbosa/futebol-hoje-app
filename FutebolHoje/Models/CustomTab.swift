bf030a46fd6f5d927709a68251e234b4639f6d77
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-10-20 19:24:18 -0300

    feat: create custom tab bar

---
+++ FutebolHoje/Models/CustomTab.swift
@@ -0,0 +1,40 @@
+//
+//  CustomTab.swift
+//  FutebolHoje
+//
+//  Created by Daniel Braga Barbosa on 20/10/25.
+//
+
+import Foundation
+
+enum CustomTab: String, CaseIterable
+{
+    case home = "Home"
+    case notifications = "Notifications"
+//    case settings = "Settings"
+    
+    var symbol: String
+    {
+        switch self
+        {
+            case .home: return "house"
+            case .notifications: return "bell"
+//            case .settings: return "gearshape"
+        }
+    }
+    
+    var activeSymbol: String
+    {
+        switch self
+        {
+            case .home: return "plus"
+            case .notifications: return "tray.full.fill"
+//            case .settings: return "cloud.moon.fill"
+        }
+    }
+    
+    var index: Int
+    {
+        Self.allCases.firstIndex(of: self) ?? 0
+    }
+}
---
+++ FutebolHoje/Views/CustomTabBarView.swift
@@ -0,0 +1,56 @@
+//
+//  CustomTabBarView.swift
+//  FutebolHoje
+//
+//  Created by Daniel Braga Barbosa on 20/10/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomTabBarView: View
+{
+    @Binding var activeTab: CustomTab
+
+    var body: some View
+    {
+        HStack(spacing: 10)
+        {
+            GeometryReader
+            {
+                CustomTabBar(size: $0.size, activeTab: $activeTab)
+                { tab in
+                    VStack(spacing: 3)
+                    {
+                        Image(systemName: tab.symbol)
+                            .font(.title3)
+
+                        Text(tab.rawValue)
+                            .font(.system(size: 10))
+                            .fontWeight(.medium)
+                    }
+                    .symbolVariant(.fill)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+        }
+        .frame(height: 55)
+        .padding(.horizontal, 20)
+        .background(Color(.systemBackground))
+    }
+}
+
+#Preview
+{
+    struct PreviewHost: View
+    {
+        @State var tab: CustomTab = .home
+        
+        var body: some View
+        {
+            CustomTabBarView(activeTab: $tab)
+        }
+    }
+    
+    return PreviewHost()
+}
