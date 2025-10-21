867937b33e94b0a8d039893b58228d2d8307adbd
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-10-21 20:42:20 -0300

    feat: update GamesListView

--- FutebolHoje/Views/GamesListView.swift
+++ FutebolHoje/Views/GamesListView.swift
@@ -5,41 +5,31 @@
 //  Created by Daniel Braga Barbosa on 05/08/25.
 //
 
+import Foundation
 import SwiftUI
-
-//struct GamesListView: View
-//{
-//    @StateObject var viewModel = GamesViewModel()
-
-//    var body: some View
-//    {
-//        Text("Hello, World!")
-        
-//        List(viewModel.games)
-//        { game in
-//            
-//            GameRowView(game: game)
-//        }
-//        .onAppear
-//        {
-//            viewModel.fetchGames()
-//        }
-//    }
-//}
 
 struct GamesListView: View
 {
+    @ObservedObject var viewModel: GamesViewModel
+    
     var body: some View
     {
-        // MARK: - Lista de jogos
         ScrollView
         {
             VStack(spacing: 12)
             {
-                ForEach(0..<5, id: \.self)
-                { _ in
-                    
-                    GameCardView()
+                if viewModel.todayGames.isEmpty
+                {
+                    Text("Nenhum jogo hoje.")
+                        .foregroundColor(.secondary)
+                        .padding(.top, 40)
+                }
+                else
+                {
+                    ForEach(viewModel.todayGames)
+                    { game in
+                        GameCardView(game: game)
+                    }
                 }
             }
             .padding()
@@ -49,5 +39,5 @@ struct GamesListView: View
 
 #Preview
 {
-    GamesListView()
+    GamesListView(viewModel: GamesViewModel.init())
 }
