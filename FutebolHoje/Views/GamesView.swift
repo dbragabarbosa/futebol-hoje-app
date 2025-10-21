d447ac01d5f74d3c19e4f520df8b87cd160934ba
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-10-21 20:41:11 -0300

    feat: update gamesView

--- FutebolHoje/Views/GamesView.swift
+++ FutebolHoje/Views/GamesView.swift
@@ -8,58 +8,30 @@
 import Foundation
 import SwiftUI
 
-//struct GamesView: View
-//{
-//    @StateObject private var viewModel = GamesViewModel()
-//    
-//    var body: some View
-//    {
-//        NavigationView
-//        {
-//            List(viewModel.games)
-//            { game in
-//                
-//                VStack(alignment: .leading, spacing: 6)
-//                {
-//                    Text("\(game.homeTeam) x \(game.awayTeam)")
-//                        .font(.headline)
-//                    
-//                    Text("Horário: \(game.date.formatted(date: .omitted, time: .shortened))")
-//                        .font(.subheadline)
-//                    
-//                    Text("Transmissão: \(game.broadcaster)")
-//                        .font(.footnote)
-//                        .foregroundColor(.secondary)
-//                }
-//                .padding(.vertical, 4)
-//            }
-//            .navigationTitle("Futebol Hoje")
-//        }
-//    }
-//}
-
 struct GamesView: View
 {
+    @StateObject private var viewModel = GamesViewModel()
+    
+    @State private var activeTab: CustomTab = .home
+    
     var body: some View
     {
         VStack(spacing: 0)
         {
             HeaderView()
-            
             DatePickerView()
-
             Divider()
             
-            GamesListView()
+            GamesListView(viewModel: viewModel)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color(.systemBackground))
             
             Divider()
             
-            TabsView()
+//            TabsView()
+            CustomTabBarView(activeTab: $activeTab)
         }
-        .ignoresSafeArea(edges: .bottom)
-        .background(Color(.systemGroupedBackground))
+        .background(Color(.systemBackground))
     }
 }
 
